0ac5e1b74d33af6708a14d1cead0665325ea9adf
Vinicius Mulinari Stuani <viniciusmulinari.st@gmail.com>
2022-11-28 16:18:29 -0300

    servidor

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const fileupload = require("express-fileupload");
+
+const apiRouter = require("./src/routes");
+const db = require("./src/db");
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(fileupload());
+
+server.use(express.static(__dirname + "/public"));
+
+server.use("/", apiRouter);
+
+db.authenticate();
+db.sync()
+    .then(console.log("Database connected"))
+    .catch((err) => console.log(err));
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.BASE}`);
+});
